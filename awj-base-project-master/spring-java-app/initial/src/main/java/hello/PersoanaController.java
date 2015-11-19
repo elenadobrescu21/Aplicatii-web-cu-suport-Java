f583bdaf5bdd79ef5203e26f9cb4cc00f5a14fbc
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2015-11-19 20:07:45 +0200

    tried to fix coding style ruined by git

--- awj-base-project-master/spring-java-app/initial/src/main/java/hello/PersoanaController.java
+++ awj-base-project-master/spring-java-app/initial/src/main/java/hello/PersoanaController.java
@@ -16,9 +16,9 @@ public class PersoanaController {
   private List<Persoana> persoane = new ArrayList<Persoana>();
 
   PersoanaController() {
-	Adresa a1 = new Adresa("Bucuresti", "Fraului", 18);
-	Adresa a2 = new Adresa("Brasov", "Fantanica", 32);
-	Adresa a3 = new Adresa ("Bucuresti", "Mihai Bravu", 38);
+    Adresa a1 = new Adresa("Bucuresti", "Fraului", 18);
+    Adresa a2 = new Adresa("Brasov", "Fantanica", 32);
+    Adresa a3 = new Adresa ("Bucuresti", "Mihai Bravu", 38);
     Persoana p1 = new Persoana(1, "John", a1);
     Persoana p2 = new Persoana(2, "Paul", a2);
     Persoana p3 = new Persoana(3, "Paul", a3);
@@ -45,8 +45,7 @@ public class PersoanaController {
   
   //cauta persoane dintr-un oras dat ca parametru
   @RequestMapping(value="/persoana/oras", method = RequestMethod.GET)
-  public List<Persoana> show(@RequestParam(value="oras", defaultValue="Oras") String oras){
-	
+  public List<Persoana> show(@RequestParam(value="oras", defaultValue="Oras") String oras) {
    List<Persoana> pers = new ArrayList<Persoana>();
     for(Persoana p : this.persoane) {
       if(p.getAddress().getOras().equals(oras)) {
@@ -69,33 +68,33 @@ public class PersoanaController {
   
   @RequestMapping(value="/persoana", method = RequestMethod.POST)
   public ResponseEntity create(@RequestParam(value="name", defaultValue="Elena") String name,
-		                       @RequestParam(value="oras", defaultValue="Oras") String oras,
-  							   @RequestParam(value="stada", defaultValue="Strada") String strada,
-  							   @RequestParam(value="numar", defaultValue="0") int numar) {
+		               @RequestParam(value="oras", defaultValue="Oras") String oras,
+  			       @RequestParam(value="stada", defaultValue="Strada") String strada,
+  			       @RequestParam(value="numar", defaultValue="0") int numar) {
   
-	Adresa adr = new Adresa(String.format(oras), String.format(strada), numar);
-	Persoana newPerson = new Persoana(this.persoane.size() + 1,String.format(name), adr);
-	persoane.add(newPerson);
-	String numePersoana = newPerson.getName();
-	for(Persoana p : this.persoane) {	
-		if(p.getName().equals(numePersoana)) {
-             return new ResponseEntity<String>(null, new HttpHeaders(), HttpStatus.OK);
+  Adresa adr = new Adresa(String.format(oras), String.format(strada), numar);
+  Persoana newPerson = new Persoana(this.persoane.size() + 1,String.format(name), adr);
+  persoane.add(newPerson);
+  String numePersoana = newPerson.getName();
+  for(Persoana p : this.persoane) {	
+  	if(p.getName().equals(numePersoana)) {
+        	return new ResponseEntity<String>(null, new HttpHeaders(), HttpStatus.OK);
         }
-	}
-		return new ResponseEntity<String>(null, new HttpHeaders(), HttpStatus.NOT_FOUND);	
+   }
+	return new ResponseEntity<String>(null, new HttpHeaders(), HttpStatus.NOT_FOUND);	
 }  
  
   @RequestMapping(value="/persoana/{id}", method = RequestMethod.PUT)
   public ResponseEntity update(@PathVariable("id") int id , @RequestParam(value="name", defaultValue="Updated Name") String newName,
-		 					   @RequestParam(value="oras", defaultValue="Oras") String oras,
-			                   @RequestParam(value="stada", defaultValue="Strada") String strada,
-			                   @RequestParam(value="numar", defaultValue="0") int numar) {
+		 	       @RequestParam(value="oras", defaultValue="Oras") String oras,
+			       @RequestParam(value="stada", defaultValue="Strada") String strada,
+			       @RequestParam(value="numar", defaultValue="0") int numar) {
   Adresa adr = new Adresa(String.format(oras), String.format(strada), numar);
-    for(Persoana p : this.persoane) {
+  for(Persoana p : this.persoane) {
       if(p.getId() == id) {
         p.setName(newName);
         p.setAddress(adr);
-		return new ResponseEntity<Persoana>(p, new HttpHeaders(), HttpStatus.OK);
+	return new ResponseEntity<Persoana>(p, new HttpHeaders(), HttpStatus.OK);
       }
     }
     return new ResponseEntity<String>(null, new HttpHeaders(), HttpStatus.NOT_FOUND);
